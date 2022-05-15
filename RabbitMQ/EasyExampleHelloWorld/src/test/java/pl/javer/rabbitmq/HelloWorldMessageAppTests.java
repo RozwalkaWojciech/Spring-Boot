47f1bc9281eb618c56c8bfeeec84f4b955d0c679
w.rozwal <WojciechRozwalka@medapp.pl>
2022-05-15 23:07:06 +0200

    add rabbit queue example app

---
+++ RabbitMQ/EasyExampleHelloWorld/src/test/java/pl/javer/rabbitmq/HelloWorldMessageAppTests.java
@@ -0,0 +1,13 @@
+package pl.javer.rabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class So43694619ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
